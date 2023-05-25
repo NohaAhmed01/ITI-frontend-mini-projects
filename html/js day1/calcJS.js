cb369214335905f5512493c52917a85d5d1628f4
Noha Ahmed 01 <119630724+NohaAhmed01@users.noreply.github.com>
2023-05-26 02:17:11 +0300

    Add files via upload

---
+++ html/js day1/calcJS.js
@@ -0,0 +1,5 @@
+
+let x= prompt('enter the first number: ');
+let y= prompt('enter the second number: ');
+let z= parseInt(x)+ parseInt(y); 
+console.log(x+" + "+y+" = "+ (z));
---
+++ html/js day1/first.js
@@ -0,0 +1,6 @@
+let fname= prompt("type your first name");
+let lname= prompt("type your last name");
+confirm("is your full name "+ fname +" " + lname);
+let birthYear= prompt('enter your birth year');
+alert("welcome "+fname+" "+lname+" you are "+ (2022-birthYear) +" years old");
+console.log("welcome "+fname+" "+lname+" you are "+ (2022-birthYear) +" years old");
